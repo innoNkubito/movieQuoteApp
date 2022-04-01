cb079c51213a4b8cdf9e807357bdcebe50176510
Nkubito Innocent <inkubito@vailsys.com>
2022-04-01 12:32:50 -0500

    Implement movie quote app

---
+++ README.md
@@ -0,0 +1,51 @@
+# Node Movie Quote Sample App
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+This project is a voice and SMS app built with FreeClimb that takes a phrase via call or text and says/texts back a movie quote with the
+provided phrase
+
+# Description 
+Specifically this app will:
+- Provide a user with a number to call/text 
+- The user will then call or text the number and provide a phrase
+- The text feature will allow any random phrases
+- However, the call/audio feature will only allow a specific set of phrases as specified by the Grammar
+
+# Requirements
+A [FreeClimbAccount](https://www.freeclimb.com/dashboard/signup/)
+
+A [registered application](https://docs.freeclimb.com/docs/registering-and-configuring-an-application#register-an-app)
+
+A [configured FreeClimb number](https://docs.freeclimb.com/docs/getting-and-configuring-a-freeclimb-number) assigned to your application
+
+# Tools
+- [Node.js](https://nodejs.org/en/download/) 17.5.0 or higher
+- [Yarn](https://yarnpkg.com/en/)
+
+## Setting up the Sample App
+
+1. Install the required packages
+
+    ```bash
+    yarn install
+    ```
+
+2. Create a .env file and configure the following environment variables within it:
+
+
+    | ENV VARIABLE    | DESCRIPTION                                                                                                                                                                                                                               |
+    | --------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+    | ACCOUNT_ID      | Account ID which can be found under [API credentials](https://www.freeclimb.com/dashboard/portal/account/authentication) in dashboard.                                                                                                    |
+    | API_KEY         | Authentication token which can be found under [API credentials](https://www.freeclimb.com/dashboard/portal/account/authentication) in dashboard                                                                                           |
+    | HOST_URL        | Specifies your app's url that freeclimb will use for redirects |
+    | MOVIE_API       | Specifies the movie quote api host |
+    | PORT            | Specifies the port on which the app will run (e.g. PORT=3000 means you would direct your browser to http://localhost:3000). | 
+    
+   ## Running the Sample App
+
+```bash
+yarn start
+```
+
+## Feedback & Issues
+If you would like to give the team feedback or you encounter a problem, please [contact support](https://www.freeclimb.com/support/) or [submit a ticket](https://freeclimb.com/dashboard/portal/support) in the dashboard.
---
+++ index.ts
@@ -0,0 +1,115 @@
+import dotenv from "dotenv-safe"
+import express, { Request, Response } from "express"
+import bodyParser from "body-parser"
+import freeClimbSdk from "@freeclimb/sdk"
+import fetch from "node-fetch"
+
+dotenv.config()
+const { ACCOUNT_ID, API_KEY, HOST_URL, PORT, MOVIE_API } = process.env
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+interface MovieQuote {
+    title: string
+    year: number
+    phrase: string
+}
+
+interface SmsBody {
+    from: string
+    text: string
+    to: string
+}
+
+// init FreeClimb sdk 
+const freeClimb = freeClimbSdk(ACCOUNT_ID, API_KEY)
+
+const generateQuote = async (phrase: string): Promise<MovieQuote> => {
+    // Movie quote api URL 
+    const movieQuoteUrl = `${MOVIE_API}/${encodeURIComponent(phrase)}`
+
+    // Trigger quote api request
+    const mvResponse = await fetch(movieQuoteUrl, { method: "GET" })
+    const movieQuotes: { docs: MovieQuote[] } = await mvResponse.json()
+
+    // select random  element from returned array of quotes
+    const randomIndex = Math.floor(Math.random() * (movieQuotes.docs.length - 1))
+    return movieQuotes.docs[randomIndex]
+}
+
+app.post("/incomingCall", (req: Request, res: Response<freeClimbSdk.PerCL.Command[]>) => {
+    const actionUrl = `${HOST_URL}/movieQuote`
+    const grammarFile = `${HOST_URL}/grammar`
+    const options: freeClimbSdk.PerCL.GetSpeechOptions = {
+        grammarType: "URL",
+        playBeep: true,
+        grammarRule: "Movies",
+        prompts: [
+            {
+                "Say": {
+                    "text": "Please utter a phrase."
+                }
+            }
+        ]
+    }
+    const getSpeech = freeClimb.percl.getSpeech(actionUrl, grammarFile, options)
+    res.send(freeClimb.percl.build(getSpeech))
+})
+
+// Grammar URL/download url  for GetSpeech command
+app.get("/grammar", (req: Request, res: Response) => {
+    res.download("./files/test.xml", (err) => {
+        if (err) {
+            console.log(`Error downloading the XML file: ${err.message}`)
+        }
+    })
+})
+
+app.post("/movieQuote", async (req: Request<any, any, {
+    recognitionResult: string
+    to: string
+    from: string
+}>, res: Response<freeClimbSdk.PerCL.Command[]>) => {
+    // Get Speech Recoginitio result
+    const { recognitionResult: phrase, to, from } = req.body
+    // redirect call command
+    const redirect = freeClimb.percl.redirect(`${HOST_URL}/incomingCall`)
+    if (phrase) {
+        // Movie quote api URL 
+        const randomQuote = await generateQuote(phrase)
+        if (randomQuote) {
+            const { title, phrase, year } = randomQuote
+            const movieTitle = `${title} ${year}`
+            const movieQuote = `The quote is: "${phrase}"`
+            const resPercl = freeClimb.percl.build(freeClimb.percl.say(movieTitle), freeClimb.percl.say(movieQuote), freeClimb.percl.sms(to, from, `${movieTitle}`), redirect)
+            res.send(resPercl)
+        } else {
+            res.send(freeClimb.percl.build(redirect))
+        }
+    } else {
+        res.send(freeClimb.percl.build(redirect))
+    }
+})
+
+app.post("/incomingSms", async (req: Request<any, any, SmsBody>, res: Response) => {
+    console.log(req.body)
+    const { from, to, text } = req.body
+
+    const randomQuote = await generateQuote(text)
+
+    if (randomQuote) {
+        const { title, phrase, year } = randomQuote
+        const movieTitle = `${title} ${year}`
+        const movieQuote = `the quote is: "${phrase}"`
+        await freeClimb.api.messages.create(to, from,`${movieTitle} ${movieQuote}`)
+        res.sendStatus(200)
+    } else {
+        await freeClimb.api.messages.create(to, from, "Something unexpected happened")
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Running app on: ${PORT}`)
+})
